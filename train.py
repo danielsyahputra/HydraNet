bd0a2ca565de8cce9816a38e3f60892c0d463759
danielsyahputra13 <dnlshp@gmail.com>
2022-10-25 15:34:34 +0700

    add modularize train script

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import torch
+import argparse
 import torch.nn as nn
 import torch.optim as optim
+from data.dataset import dataloader
 from utils.engine import train_model
 from models.hydranet import HydraNet
-from data.dataset import dataloader
 
-def main():
+def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = HydraNet()
     loss_fns = {
@@ -15,17 +16,43 @@ def main():
     }
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
     train_loader, val_loader = dataloader(data_dir="data/UTKFace")
+    epochs = args.epochs
+    experiment_name = args.experiment_name
+    model_dir = args.model_dir
     train_model(
         model=model,
         loaders={"train": train_loader, "val": val_loader},
         optimizer=optimizer,
         loss_fns=loss_fns,
         device=device,
-        model_dir="output/exp1",
-        experiment_name="Uniform Weight Experiment",
+        model_dir=model_dir,
+        experiment_name=experiment_name,
         model_name="Model",
-        epochs=10
+        epochs=epochs
     )
 
 if __name__=="__main__":
-    main()
+    parser = argparse.ArgumentParser(description="HydraNet Using Uncertainty to Weigh Losses")
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        metavar="N",
+        help="Number of epochs for training (default: 10)."
+    )
+    parser.add_argument(
+        "--experiment-name",
+        type=str,
+        default="Experiment All",
+        metavar="EN",
+        help="Name of Experiment in MLFLow."
+    )
+    parser.add_argument(
+        "--model-dir",
+        type="str",
+        default="exp1",
+        metavar="MD",
+        help="Name of directory for saving the model and artifacts."
+    )
+    args = parser.parse_args()
+    main(args)
